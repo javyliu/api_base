c1618d0732f67a1a2b5c8cf10e7d595e0c449173
qmliu <qmliu@pearlinpalm.com>
2021-01-21 12:22:29 +0800

    add Bcrypt to generate password hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :password_digest, presence: true
   validates :email, format: { with: /@/ }
+
+  has_secure_password
 end

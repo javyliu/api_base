f932636b986bebadb0850af9968a1bb1602c68cc
qmliu <qmliu@pearlinpalm.com>
2021-01-21 16:22:07 +0800

    add the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,22 @@ class Api::V1::UsersController < ApplicationController
     render json: User.find(params[:id])
   end
 
+  #post /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+
+  end
+
+
+  private
+  #only allow a trusted parameter 'white list' through.
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end

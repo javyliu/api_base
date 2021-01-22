57a6772b03c4f5da9c751d00f222b263a36ae91b
qmliu <qmliu@pearlinpalm.com>
2021-01-22 17:44:46 +0800

    add user relationship to product serializer

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -11,7 +11,8 @@ class Api::V1::ProductsController < ApplicationController
 
 
   def show
-    render json: ProductSerializer.new(Product.find(params[:id])).serializable_hash.to_json
+    options = {include: [:user]}
+    render json: ProductSerializer.new(@product, options).serializable_hash.to_json
   end
 
 

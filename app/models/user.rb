4846432780343593bc7923de93de02772632379b
qmliu <qmliu@pearlinpalm.com>
2021-01-26 17:12:54 +0800

    generate orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :products, dependent: :destroy
 
   has_secure_password
+
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end

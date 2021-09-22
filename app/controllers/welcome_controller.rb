f18b52f6a8740780fa5b42a084ee6e45a01f664c
qmliu <qmliu@pearlinpalm.com>
2021-09-22 17:08:42 +0800

    add routes list in welcome/index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    render json: `rails routes -g /api/v1`
+    render json: `rails routes|  awk 'BEGIN{FIELDWIDTHS ="40 7 98 300"}/api\\/v1/{print $2  $4}'`
   end
 end
